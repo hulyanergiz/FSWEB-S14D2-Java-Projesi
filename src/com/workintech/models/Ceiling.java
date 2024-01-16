43f77eff3e1ce0d6387f354d2512485b035a2204
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-16 18:34:57 +0300

    creates Ceiling class

---
+++ src/com/workintech/models/Ceiling.java
@@ -0,0 +1,34 @@
+package com.workintech.models;
+
+import com.workintech.enums.PaintColor;
+
+public class Ceiling {
+    private int height;
+    private PaintColor paintedColor;
+
+    public Ceiling(int height, PaintColor paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public PaintColor getPaintedColor() {
+        return paintedColor;
+    }
+
+    @Override
+    public String toString() {
+        return "Ceiling{" +
+                "height=" + height +
+                ", paintedColor=" + paintedColor +
+                '}';
+    }
+
+    public void create(){
+        System.out.println(this.toString());
+
+    }
+}
